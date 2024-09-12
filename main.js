666d45b842a16989c38bfec91a1c23e4790c84dc
najwa <NajwaBuayjan@gmail.com>
2024-09-12 10:53:19 +0300

    solving the lab

---
+++ main.js
@@ -0,0 +1,8 @@
+let but1 = document.getElementById('but1');
+but1.classList.add('hover-text');
+
+let but2 = document.getElementById('but2');
+but2.classList.add('hover-text2');
+
+let but3 = document.getElementById('but3');
+but3.classList.add('hover-text3');
